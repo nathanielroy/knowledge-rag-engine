954381f6ea8583f7fc7cc2560e73948b3f0549a3
nathanielroy <nathanielroy97@gmail.com>
2026-06-18 22:16:13 +0530

    Initial commit

---
+++ src/main/java/com/knowledge/rag/engine/KnowledgeRagEngineApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge.rag.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeRagEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeRagEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowledge/rag/engine/KnowledgeRagEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledge.rag.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeRagEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
